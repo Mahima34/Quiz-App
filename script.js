5122cd6648e3fa0d2eaa95d3819c4d55b375c39e
Mahima Rajput <135412470+Mahima34@users.noreply.github.com>
2025-07-17 14:10:00 +0530

    add json data

---
+++ script.js
@@ -0,0 +1,24 @@
+//quiz data
+
+const quizData = [
+   {
+      question: "What does HTML stand for?",
+      options:[
+         "Hyper Text Markup Language",
+         "Hyperlinks and Text Markup Language",
+         "Hyper Text Machine Language",
+         "High-Level Text Markup Language"
+      ],
+      correct:0,
+   },
+   {
+      questions: "What will typeof NaN return?",
+      options:[
+         "Nan",
+         "number",
+         "undefined",
+         "object"
+      ],
+      correct: 1,
+   }
+]
