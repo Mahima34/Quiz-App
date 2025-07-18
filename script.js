c1926934aed2c5fe549995b287b10c5f5a146c8a
Mahima Rajput <135412470+Mahima34@users.noreply.github.com>
2025-07-18 22:20:28 +0530

    add new data

--- script.js
+++ script.js
@@ -20,5 +20,95 @@ const quizData = [
          "object"
       ],
       correct: 1,
+   },
+   {
+      questions: "Which HTML tag is used to define a paragraph?",
+      options: [
+         "<p>",
+         "<paragraph>",
+         "<div>",
+         "<section>"
+      ],
+      correct: 0
+   },
+   {
+      questions: "Which of the following is a JavaScript framework?",
+      options: [
+         "Django",
+         "Flask",
+         "React",
+         "Ruby on Rails"
+      ],
+      correct: 2
+   },
+   {
+      questions: "What is the purpose of the 'this' keyword in JavaScript?",
+      options: [
+         "To refer to the global object",
+         "To refer to the current function",
+         "To refer to the current object",
+         "To refer to the previous object"
+      ],
+      correct: 2
+   },
+   {
+      questions: "Which of the following is not a JavaScript data type?",
+      options: [
+         "String",
+         "Boolean",
+         "Number",
+         "Character"
+      ],
+      correct: 3
+   },
+   {
+      questions: "What does CSS stand for?",
+      options: [
+         "Cascading Style Sheets",
+         "Creative Style Sheets",
+         "Computer Style Sheets",
+         "Cascading Simple Sheets"
+      ],
+      correct: 0
+   },
+   {
+      questions: "Which HTML attribute is used to define inline styles?",
+      options: [
+         "style",
+         "class",
+         "id",
+         "styles"
+      ],
+      correct: 0
+   },
+   {
+      questions: "What is the purpose of the 'let' keyword in JavaScript?",
+      options: [
+         "To declare a variable with block scope",
+         "To declare a constant variable",
+         "To declare a global variable",
+         "To declare a function"
+      ],
+      correct: 0
+   },
+   {
+      questions: "How do you write a comment in CSS?",
+      options:[
+         "/* This is a comment */",
+         "// This is a comment",
+         "<!-- This is a comment -->",
+         "# This is a comment"   
+      ], 
+      correct: 0
+   },
+   {
+      questions: " Which hook is used in React to manage side effects?",
+      options: [
+         "useState",
+         "useEffect",
+         "useContext",
+         "useReducer"
+      ],
+      correct: 1
    }
 ]
